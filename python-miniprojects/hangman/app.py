9f003cf835bf21755abf381772b927f0bcec5ba8
justin-pyne <jpyne.justin@gmail.com>
2023-08-30 13:54:57 -0700

    finish hangman

--- python-miniprojects/hangman/app.py
+++ python-miniprojects/hangman/app.py
@@ -22,12 +22,39 @@ def hangman():
     alphabet = set(string.ascii_uppercase)
     used_letters = set() #set of guessed letters
 
-    while len(word_letters) > 0:
+    lives = 6
+
+    while len(word_letters) > 0 and lives > 0:
         
+        print("You have", lives, "lives and you have used these letters: ", " ".join(used_letters))
+
+        word_list = [letter if letter in used_letters else "-" for letter in word]
+
+        print("The current word is: ", " ".join(word_list))
+
 
         guess = input("Guess a letter: ").upper()
-        if guess in word_letters:
-            pass
+        if guess in alphabet - used_letters:
+            used_letters.add(guess)
+            if guess in word_letters:
+                word_letters.remove(guess)
+            else:
+                lives -= 1
+
+        elif guess in used_letters:
+            print("You have already used this letter. Please try again.")
+        
+        else:
+            print("You have entered an invalid character. Please try again.")
+    
+    if lives == 0:
+        print("You lose! The word was:", word)
+    else:
+        print("You win!")
+        
+    
+            
+            
 
 
 
