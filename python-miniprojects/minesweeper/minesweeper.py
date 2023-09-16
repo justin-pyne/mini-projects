d403f359cd1561dd97bd8526ddb98e46d4fa5c6b
justin-pyne <jpyne.justin@gmail.com>
2023-09-16 20:04:57 +0200

    assign val and get bomb count

--- python-miniprojects/minesweeper/minesweeper.py
+++ python-miniprojects/minesweeper/minesweeper.py
@@ -8,6 +8,7 @@ class Board:
 
         # create the board
         self.board = self.make_new_board()
+        self.assign_values_to_board()
 
         # init a set to track uncovered locations
         self.dug = set() # (row, col) tuples in this set
@@ -31,19 +32,53 @@ class Board:
 
         return board
     
+    def assign_values_to_board(self):
+
+        for r in range(self.dim_size):
+            for c in range(self.dim_size):
+                if self.board[r][c] == '*':
+                    continue
+                self.board[r][c] = self.get_neighbor_bombs(r, c)
+
+    def get_neighbor_bombs(self, row, col):
+        
+        num_neighbor_bombs = 0
+
+        for r in range(max(0, row-1), min(self.dim_size-1, (row+1)+1)):
+            for c in range(max(0, col-1), min(self.dim_size-1, (col+1)+1)):
+                if r == row and c == col:
+                    continue
+                if self.board[r][c] == '*':
+                    num_neighbor_bombs += 1
+
+        return num_neighbor_bombs
     
+    def dig(self, row, col):
+        # True if successful dig, False if hit bomb
 
+        # Possibilities:
+        # hit a bomb -> game over
+        # dig at a location with neighboring bombs -> finish dig
+        # dig at a location with no neighboring bombs -> recursively dig neighbors
 
-
+        
 
 
 
 #play the game
 def play(dim_size=10, num_bombs=10):
     # Step 1: create the board and plant the bombs
+    board = Board(dim_size, num_bombs)
+
     # Step 2: show the user the board and ask for where they want to dig
+
+
     # Step 3a: if the location is a bomb, show game over
+
+
     # Step 3b: if the location is not a bomb, dig recursively until each square is at least next to a bomb.
+
+
     # Step 4: repeat steps 2 and 3 until there are no more spots -> WIN!
-    pass
+    
 
