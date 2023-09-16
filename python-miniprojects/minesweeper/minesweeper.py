eecbc26754ea99e038bf19c4cc7952109355b24e
justin-pyne <jpyne.justin@gmail.com>
2023-09-16 20:33:51 +0200

    need bugfix

--- python-miniprojects/minesweeper/minesweeper.py
+++ python-miniprojects/minesweeper/minesweeper.py
@@ -1,4 +1,5 @@
 import random
+import re
 
 # create a board object to represent a game
 class Board:
@@ -98,7 +99,7 @@ class Board:
             )
         
         indicies = [i for i in range(self.dim_size)]
-        indicies_row = '  '
+        indicies_row = '   '
         cells = []
         for idx, col in enumerate(indicies):
             format = '%-' + str(widths[idx]) + "s"
@@ -136,5 +137,27 @@ def play(dim_size=10, num_bombs=10):
 
 
     # Step 4: repeat steps 2 and 3 until there are no more spots -> WIN!
-    
+    safe = True
 
+    while len(board.dug) < board.dim_size**2 - num_bombs:
+        print(board)
+        user_input = re.split(',(\\s)*',input("Where would you like to dig? Input as row, col: "))
+        row, col = int(user_input[0]), int(user_input[-1])
+
+        if row < 0 or row >= board.dim_size or col < 0 or col >= dim_size:
+            print("Invalid location. Try again.")
+            continue
+
+        safe = board.dig(row, col)
+        if not safe:
+            break
+    
+    if safe:
+        print("Congratulations, you win!")
+    else:
+        print('Game over!')
+        board.dug = [(r, c) for r in range(board.dim_size) for c in range(board.dim_size)]
+        print(board)
+
+if __name__ == '__main__':
+    play()
